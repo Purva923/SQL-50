7fb373148714ab89859ca00552f65bc48afff6dd
Purva923 <purvabagul923@gmail.com>
2026-08-26 20:14:01 +0530

    Time: 595 ms (93.58%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -14,3 +14,14 @@ JOIN (
 ) f
 ON d.customer_id = f.customer_id
 AND d.order_date = f.first_order_date;
+
+
+/*SELECT ROUND(COUNT(if(order_date=min_od AND customer_pref_delivery_date=min_cpdd )*100/COUNT(DISTINCT customer_id)),2)
+AS immediate_percentage
+FROM Delivery AS d
+JOIN (SELECT customer_id,MIN(order_date) AS min_od ,MIN(customer_pref_delivery_date) 
+    AS min_cpdd
+    FROM Delivery
+    GROUP BY customer_id;) AS t
+ON d.customer_id=t.customer_id
+GROUP BY customer_id;*/
