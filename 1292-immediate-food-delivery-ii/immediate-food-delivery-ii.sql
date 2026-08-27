e96b149d7433f3d0b045e295a9e650991f492234
Purva923 <purvabagul923@gmail.com>
2026-08-27 08:28:38 +0530

    Time: 723 ms (43.84%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,27 +1,20 @@
-SELECT 
-    ROUND(
-        SUM(d.order_date = d.customer_pref_delivery_date) * 100.0
-        / COUNT(*),
-        2
-    ) AS immediate_percentage
-FROM Delivery d
-JOIN (
-    SELECT 
-        customer_id,
-        MIN(order_date) AS first_order_date
+SELECT ROUND(
+            SUM(
+                IF(
+                    d.order_date=t.min_order_date AND d.order_date=d.customer_pref_delivery_date,1,0
+                    )
+                )*100/SUM(
+                        IF(
+                            d.order_date=t.min_order_date,1,0
+                        )
+                ),2
+            )  AS immediate_percentage
+FROM delivery AS d
+JOIN (SELECT customer_id,
+    MIN(order_date) AS min_order_date 
     FROM Delivery
-    GROUP BY customer_id
-) f
-ON d.customer_id = f.customer_id
-AND d.order_date = f.first_order_date;
+    GROUP BY customer_id) AS t 
+ON t.customer_id=d.customer_id;
 
 
-/*SELECT ROUND(COUNT(if(order_date=min_od AND customer_pref_delivery_date=min_cpdd )*100/COUNT(DISTINCT customer_id)),2)
-AS immediate_percentage
-FROM Delivery AS d
-JOIN (SELECT customer_id,MIN(order_date) AS min_od ,MIN(customer_pref_delivery_date) 
-    AS min_cpdd
-    FROM Delivery
-    GROUP BY customer_id;) AS t
-ON d.customer_id=t.customer_id
-GROUP BY customer_id;*/
+
