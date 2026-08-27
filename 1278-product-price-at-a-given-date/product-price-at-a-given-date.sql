63f0ffec4565a7eb91ed3f596437fbf143d303e5
Purva923 <purvabagul923@gmail.com>
2026-08-27 16:44:38 +0530

    Time: 923 ms (12.01%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,16 @@
+SELECT p.product_id,
+       IFNULL(x.new_price, 10) AS price
+FROM (SELECT DISTINCT product_id FROM Products) AS p
+LEFT JOIN (
+    SELECT p1.product_id, p1.new_price
+    FROM Products p1
+    JOIN (
+        SELECT product_id, MAX(change_date) AS max_change_date
+        FROM Products
+        WHERE change_date <= '2019-08-16'
+        GROUP BY product_id
+    ) p2
+    ON p1.product_id = p2.product_id
+    AND p1.change_date = p2.max_change_date
+) AS x
+ON p.product_id = x.product_id;
