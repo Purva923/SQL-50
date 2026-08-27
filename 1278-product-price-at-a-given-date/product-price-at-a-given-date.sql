d8750c4adce155f3414241b7f0f6c68f23c40208
Purva923 <purvabagul923@gmail.com>
2026-08-27 17:08:33 +0530

    Time: 456 ms (91.43%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,16 +1,16 @@
-SELECT p.product_id,
-       IFNULL(x.new_price, 10) AS price
-FROM (SELECT DISTINCT product_id FROM Products) AS p
-LEFT JOIN (
-    SELECT p1.product_id, p1.new_price
-    FROM Products p1
-    JOIN (
-        SELECT product_id, MAX(change_date) AS max_change_date
-        FROM Products
-        WHERE change_date <= '2019-08-16'
-        GROUP BY product_id
-    ) p2
-    ON p1.product_id = p2.product_id
-    AND p1.change_date = p2.max_change_date
-) AS x
-ON p.product_id = x.product_id;
+SELECT p1.product_id,
+       IF(p2.max_change_date IS NULL, 10, p1.new_price) AS price
+FROM Products AS p1
+INNER JOIN (
+    SELECT product_id, MAX(change_date) AS max_change_date
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+) AS p2
+ON p1.product_id = p2.product_id
+AND p1.change_date = p2.max_change_date
+
+UNION ALL
+SELECT product_id,10 AS price FROM Products
+GROUP BY product_id
+HAVING MIN(change_date)>'2019-08-16';
