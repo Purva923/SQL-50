ded4e11cceaa33f7b90246c79f2859657a8a376b
Purva923 <purvabagul923@gmail.com>
2026-08-29 11:42:29 +0530

    Time: 264 ms (92.65%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,7 +1,9 @@
 # Write your MySQL query statement below
-SELECT(
+/*SELECT(
     SELECT salary FROM Employee
     GROUP BY salary 
     ORDER BY salary DESC
     LIMIT 1,1
-) AS SecondHighestSalary;
+) AS SecondHighestSalary;*/
+
+select max(salary) as SecondHighestSalary from Employee where salary < (select max(salary) from Employee);
